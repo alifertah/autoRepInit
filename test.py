026a808343a5ca42e2717948000aefb64058d014
alifertah <alifertah42@gmail.com>
2023-12-10 17:21:40 +0100

    feat : creating a github repo by passing the token as first argument and repo name as second argument

--- test.py
+++ test.py
@@ -1,10 +1,11 @@
 import requests
 import sys
 from requests.auth import HTTPBasicAuth
-if len(sys.argv) != 2:
+if len(sys.argv) != 3:
     print("WRONG ARGUMENTS")
-elif len(sys.argv) == 2:
+elif len(sys.argv) == 3:
     token = sys.argv[1]
+    repo_name = sys.argv[2]
     def create_repo(username, password, repo_name):
         url = "https://api.github.com/user/repos"
         data = {
@@ -27,6 +28,6 @@ elif len(sys.argv) == 2:
     if __name__ == "__main__":
         github_username = "alifertah"
         github_password = token
-        repository_name = "test"
+        repository_name = repo_name
 
         create_repo(github_username, github_password, repository_name)
